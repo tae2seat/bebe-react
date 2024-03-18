d4b53ea4f59954cef8bd4ac35c56ae4912a1cb67
hye-yeong <ghdwls69@naver.com>
2024-03-18 13:04:36 +0900

    axios instance 설정

--- src/api/axios/index.js
+++ src/api/axios/index.js
@@ -1,1 +1,33 @@
 import axios from "axios";
+
+export const authApi = axios.create({
+  baseURL: `${import.meta.env.VITE_BASE_URL}auth`,
+});
+
+export const loggedApi = axios.create({
+  baseURL: `${import.meta.env.VITE_BASE_URL}`,
+  Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+});
+
+loggedApi.interceptors.request.use(
+  function (config) {
+    const accessToken = localStorage.getItem("accessToken");
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+loggedApi.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response?.status === 404) {
+      window.location.replace("/notfound");
+    }
+    return Promise.reject(error);
+  }
+);
