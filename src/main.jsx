32bb69fe70cf87a18b2cad14e1d7caf3445d2971
hye-yeong <ghdwls69@naver.com>
2024-03-18 13:05:59 +0900

    notfound 페이지추가

--- src/main.jsx
+++ src/main.jsx
@@ -29,6 +29,10 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "/notfound",
+        element: <NotFound />,
+      },
+      {
         path: "/login",
         element: <Login />,
       },
