e236974b14c6a7ca23ce3a641c9e1f094685c915
hye-yeong <ghdwls69@naver.com>
2024-03-18 14:02:20 +0900

    service card 추가

---
+++ src/components/company/Service.jsx
@@ -0,0 +1,9 @@
+export default function Service() {
+  return (
+    <section>
+      <h1>Service</h1>
+      <div>태그 버튼</div>
+      <div>태그 내용</div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/company/Banner";
+import Service from "../components/company/Service";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Service />
     </>
   );
 }
