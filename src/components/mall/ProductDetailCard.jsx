6e6b7b409f5f1c99ae57dd43e9f954b9c6fdbfe8
hye-yeong <ghdwls69@naver.com>
2024-03-19 11:58:15 +0900

    Product get

---
+++ src/components/mall/ProductDetailCard.jsx
@@ -0,0 +1,10 @@
+export default function ProductDetail() {
+  return (
+    <div>
+      <img />
+      <div>
+        <h3></h3>
+      </div>
+    </div>
+  );
+}
--- src/pages/mall/ProductDetail.jsx
+++ src/pages/mall/ProductDetail.jsx
@@ -1,3 +1,33 @@
+import ProductDetailCard from "../../components/mall/ProductDetailCard";
+import { useEffect, useState } from "react";
+import { loggedApi } from "../../api/axios";
+import { useParams } from "next/navigation";
+
 export default function ProductDetail() {
-  return <h1>Product Detail</h1>;
+  const param = useParams();
+  const [product, setProduct] = useState("");
+
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
+  const getProductDetail = async () => {
+    try {
+      const response = await loggedApi.get(`mall/item/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      setProduct(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <h1>제품 상세 페이지</h1>
+      <ProductDetailCard />
+    </section>
+  );
 }
