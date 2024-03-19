2701179b86d743e1903315169a080467e1a981de
hye-yeong <ghdwls69@naver.com>
2024-03-19 19:48:24 +0900

    css 기초설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <>
+    <div className=" max-w-[1280px] m-auto text-center ">
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 }
 
