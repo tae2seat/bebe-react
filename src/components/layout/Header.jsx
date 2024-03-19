2b0f79b68c45412f848c4f0bfc8a626a86f2c00d
hye-yeong <ghdwls69@naver.com>
2024-03-19 20:11:41 +0900

    nav 반응형

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,8 +4,8 @@ import logo from "../../assets/logo.png";
 export default function Header() {
   return (
     <section className="flex justify-between items-center">
-      <img className="w-24 bg-red-300" src={logo} alt="logo" />
-      <nav className="flex gap-2  bg-fuchsia-200 mr-2 mt-1">
+      <img className="w-24 sm:w-32" src={logo} alt="logo" />
+      <nav className="flex gap-2 sm:gap-4 sm:text-xl mr-2 sm:mr-4 mt-1 sm:mt-2">
         <Link to="/">Home</Link>
         <Link to="/kit">Kit</Link>
         <Link to="/products">Mall</Link>
