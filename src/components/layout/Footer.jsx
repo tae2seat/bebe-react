caec65397030da231d53b7b522b1792d2286ce42
hye-yeong <ghdwls69@naver.com>
2024-03-20 12:58:21 +0900

    footer 생성

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,40 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className=" bg-black text-white ">
+      <div>
+        <div>
+          <h2>Bebe Family</h2>
+          <p>다이어리</p>
+          <p>키트</p>
+          <p>베베몰</p>
+          <p>베베케어</p>
+          <p>베베AI</p>
+        </div>
+        <div>
+          <h2>인재영입</h2>
+          <p>직무 소개</p>
+          <p>회사 지원하기</p>
+        </div>
+        <div>
+          <h2>고객센터</h2>
+          <p>모바일 고객센터</p>
+          <p>유선 고객센터</p>
+        </div>
+        <div>
+          <h2>문의</h2>
+          <p>사업 제휴</p>
+          <p>광고 문의</p>
+        </div>
+      </div>
+
+      <div>
+        <h2>베베(주)</h2>
+        <div>
+          <p>사업자 등록번호: 000-00-00000</p>
+          <p>대표이사: 김 혜 영 </p>
+        </div>
+        <p>https://diary.mybebe.net/</p>
+      </div>
+    </div>
+  );
 }
