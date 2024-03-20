d188835220a5aa49296b7a39d7032f443fe372ef
hye-yeong <ghdwls69@naver.com>
2024-03-20 16:27:00 +0900

    footer 생성

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,39 +1,37 @@
 export default function Footer() {
   return (
-    <div className=" bg-black text-white ">
-      <div>
+    <div className="flex flex-col justify-around bg-black text-white p-5 ">
+      <div className="flex justify-around items-start">
         <div>
-          <h2>Bebe Family</h2>
-          <p>다이어리</p>
-          <p>키트</p>
-          <p>베베몰</p>
-          <p>베베케어</p>
-          <p>베베AI</p>
+          <h2 className="text-sm mb-2">Bebe Family</h2>
+          <p className="text-xs">다이어리</p>
+          <p className="text-xs">키트</p>
+          <p className="text-xs">베베몰</p>
         </div>
         <div>
-          <h2>인재영입</h2>
-          <p>직무 소개</p>
-          <p>회사 지원하기</p>
+          <h2 className="text-sm mb-2">인재영입</h2>
+          <p className="text-xs">직무 소개</p>
+          <p className="text-xs">회사 지원하기</p>
         </div>
         <div>
-          <h2>고객센터</h2>
-          <p>모바일 고객센터</p>
-          <p>유선 고객센터</p>
+          <h2 className="text-sm mb-2">고객센터</h2>
+          <p className="text-xs">모바일 고객센터</p>
+          <p className="text-xs">유선 고객센터</p>
         </div>
         <div>
-          <h2>문의</h2>
-          <p>사업 제휴</p>
-          <p>광고 문의</p>
+          <h2 className="text-sm mb-2">문의</h2>
+          <p className="text-xs">사업 제휴</p>
+          <p className="text-xs">광고 문의</p>
         </div>
       </div>
 
-      <div>
-        <h2>베베(주)</h2>
+      <div className="mt-4 text-end pr-4">
+        <h2 className="text-sm mb-2">베베(주)</h2>
         <div>
-          <p>사업자 등록번호: 000-00-00000</p>
-          <p>대표이사: 김 혜 영 </p>
+          <p className="text-xs">사업자 등록번호: 000-00-00000</p>
+          <p className="text-xs">대표이사: 김 혜 영 </p>
         </div>
-        <p>https://diary.mybebe.net/</p>
+        <p className="text-xs">https://diary.mybebe.net/</p>
       </div>
     </div>
   );
