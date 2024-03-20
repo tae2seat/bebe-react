1784c6f37ce427212fff96e08137ba3b52ebd163
hye-yeong <ghdwls69@naver.com>
2024-03-20 12:39:35 +0900

    Icons Box info

--- src/components/company/Icons.jsx
+++ src/components/company/Icons.jsx
@@ -3,6 +3,15 @@ import IconsBox from "./iconCard/IconsBox";
 export default function Icons() {
   return (
     <section>
+      <h1 className="text-[26px]">Brand Value</h1>
+      <p>
+        "베베"는 우리의 서비스를 누구나 잘 이해하고
+        <br />
+        가치를 공유하며, 나아가기를 희망합니다.
+        <br />
+        우리의 <span>아이콘</span>은 그것에 대한 첫 제시이며,
+        <br />더 쉽고 편한 시작이 되기를 바랍니다.
+      </p>
       <h1 className="text-[26px]">Icons</h1>
       <IconsBox />
     </section>
