e8862929cb6a5217d993eed837428eb0ee2648fb
hye-yeong <ghdwls69@naver.com>
2024-03-21 16:49:37 +0900

    변경

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="max-w-[1024px] min-w-[320px] m-auto text-center ">
+    <div className="max-w-[1024px] min-w-[320px] mx-auto text-center ">
       <Header />
       <Outlet />
       <Footer />
---
+++ src/components/mall/MyCart.jsx
@@ -0,0 +1,3 @@
+export default function MyCart() {
+  return <h1>나의 장바구니</h1>;
+}
